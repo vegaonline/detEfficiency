e753afaa561da6fd4c269545bc30a6543e4e5cc3
Abhijit Bhattacharyya <abhihere@gmail.com>
2016-09-08 17:34:43 +0530

    some modification...

--- include/main.h
+++ include/main.h
@@ -9,45 +9,45 @@
 #include <chrono>
 
 #define EPSA 1.0e-1
-#define PI 2.0 * asin ( 1.0 )
+#define PI 2.0 * asin(1.0)
 #define PIby2 0.5 * PI
 #define PI2 2.0 * PI
 
-#define x2( x ) ( x * x )
+#define x2(x) (x * x)
 #define sqrt(x) pow(x, 0.5)
 #define SGN(x) (x > 0) - (x < 0)
-#define isAT(x1, x2) ((x1 >= (x2 - EPSA)) && (x1 <= (x2 + EPSA)))  //  search for X1 within x2+/ EPSA
+#define isAT(x1, x2) ((x1 >= (x2 - EPSA)) && (x1 <= (x2 + EPSA))) //  search for X1 within x2+/ EPSA
 
 #define ev2J 1.60217656535e-19
 #define amu 1.66053904020e-24     // g
 #define eCharge 1.60217656535e-19 // Coulomb
-#define cm2m 0.01; // cm to meter
-#define cm2mm 10.0; // cm to mm
+#define cm2m 0.01;                // cm to meter
+#define cm2mm 10.0;               // cm to mm
 
 std::default_random_engine rngen;
-unsigned seedRD = std::chrono::system_clock::now ().time_since_epoch ().count ();
-std::mt19937_64 mySeed ( seedRD );
+unsigned seedRD = std::chrono::system_clock::now().time_since_epoch().count();
+std::mt19937_64 mySeed(seedRD);
 
 // declare some important global variable
 unsigned int numParticle = 4;
-std::string particleArray [] = {"alpha", "triton", "deuteron", "proton"};
+std::string particleArray[] = {"alpha", "triton", "deuteron", "proton"};
 
 // createdetector node vector
-std::vector<std::vector<double> > detNodes;
-
-
+std::vector<std::vector<double>> detNodes;
 
 class Vector3 {
-  double v1;
-  double v2;
-  double v3;
- public:
- Vector3(double a, double b, double c) : v1(a), v2(b), v3(c){}
+    double v1;
+    double v2;
+    double v3;
+
+  public:
+    Vector3(double a, double b, double c) : v1(a), v2(b), v3(c)
+    {
+    }
 };
 
-class particle
-{
- public:
+class particle {
+  public:
     std::string name;
     double massNumU;
     double charge;
@@ -58,30 +58,30 @@ class particle
     double BE;           // eV
     double massNumG = massNumU * amu;
     double chargeCoul = charge * eCharge;
-    double posX;         // x position on detector
-    double posY;         // y position on detector
- public:
- particle(std::string name, double mU, double ch, double en, double tht, double phi, double ex, double binEn) 
-   : name(name),massNumU(mU), charge(ch), energy(en), angle_theta(tht), angle_phi(phi), excessEnergy(ex), BE(binEn){}
-  
+    double posX; // x position on detector
+    double posY; // y position on detector
+  public:
+    particle(std::string name, double mU, double ch, double en, double tht, double phi, double ex, double binEn)
+        : name(name), massNumU(mU), charge(ch), energy(en), angle_theta(tht), angle_phi(phi), excessEnergy(ex),
+          BE(binEn)
+    {
+    }
 };
 
-class beam
-{
+class beam {
     std::string name;
-    double ELAB;   // energy
+    double ELAB; // energy
     double ECM;
-    Vector3 PLAB;  // 3 momentum
+    Vector3 PLAB; // 3 momentum
     Vector3 PCM;
-    Vector3 VLAB;  // 3 velocity
+    Vector3 VLAB; // 3 velocity
     Vector3 VCM;
-    Vector3 pos;   // position
-    double beamRadius;  // radius of beam
-    double charge;      // charge
+    Vector3 pos;       // position
+    double beamRadius; // radius of beam
+    double charge;     // charge
 };
 
-class target
-{
+class target {
     std::string name;
     double massNumU;
     double atNum;
@@ -91,137 +91,164 @@ class target
     double thick;
     double SArea = length * breadth; // surface area
 
- public:
- target(std::string name, double mU, double atN, double den, double len, double br, double thk) 
-   : name(name), massNumU(mU), atNum(atN), density(den), length(len), breadth(br), thick(thk){}
+  public:
+    target(std::string name, double mU, double atN, double den, double len, double br, double thk)
+        : name(name), massNumU(mU), atNum(atN), density(den), length(len), breadth(br), thick(thk)
+    {
+    }
 };
 
-class detector
-{
- public:
+class stripdetector {
+  public:
     std::string name;
     double lenX;
     double lenY;
     double halfX = 0.5 * lenX;
     double halfY = 0.5 * lenY;
     double thick;
+    double gap;
     double dx;
     double dy;
     int numStripX;
     int numStripY;
- public:
-    double SArea(){return lenX * lenY;} // surface area;
-    detector(std::string name, double lx, double ly, double thk, int nx, int ny) 
-   : name(name), lenX(lx), lenY(ly), thick(thk), numStripX(nx), numStripY(ny){}
+
+  public:
+    double SArea()
+    {
+        return lenX * lenY;
+    } // surface area;
+    detector(std::string name, double lx, double ly, double thk, double gp, int nx, int ny)
+        : name(name), lenX(lx), lenY(ly), thick(thk), gap(gp), numStripX(nx), numStripY(ny)
+    {
+    }
 };
 
 // generate particle pair within a solid angle
 
-void setPos(double tht, double phi, double hval, double dist, double& xval, double& yval) {
-  double  tht1 = tht;
-  int isgn = SGN(tht);
+void setPos(double tht, double phi, double hval, double dist, double& xval, double& yval)
+{
+    double tht1 = tht;
+    int isgn = SGN(tht);
 
-  tht = std::abs(tht);
-  xval = dist * tan(tht) * cos(phi);    // dist * tht * cos(phi);
-  yval = dist * tan(tht) * sin(phi);    // dist * tht * sin(phi);
-  xval  *= isgn;
-  yval *= isgn;
+    tht = std::abs(tht);
+    xval = dist * tan(tht) * cos(phi); // dist * tht * cos(phi);
+    yval = dist * tan(tht) * sin(phi); // dist * tht * sin(phi);
+    xval *= isgn;
+    yval *= isgn;
 }
 
-void gen_Particle (int& p1, int& p2,  double detTheta, double lx, double ly, double hval, double dist, double& xval, double& yval) { 
-  double thistht = 9999.99, thisphi = 9999.99;
-  std::uniform_int_distribution<> partDist(0, (int)numParticle-1);
-  std::uniform_real_distribution<double> angleDist2(0.0, PI2);
-  std::uniform_real_distribution<double> angleDist1(-detTheta, detTheta);
+void gen_Particle(int& p1,
+                  int& p2,
+                  double detTheta,
+                  double lx,
+                  double ly,
+                  double hval,
+                  double dist,
+                  double& xval,
+                  double& yval)
+{
+    double thistht = 9999.99, thisphi = 9999.99;
+    std::uniform_int_distribution<> partDist(0, (int)numParticle - 1);
+    std::uniform_real_distribution<double> angleDist2(0.0, PI2);
+    std::uniform_real_distribution<double> angleDist1(-detTheta, detTheta);
 
-  do {
-    p1 = partDist(mySeed);
-    p2 = partDist(mySeed);
-  } while (p1 !=0);
-  
-  do {
     do {
-      thistht = angleDist1(mySeed);      // rngen);
-    } while (std::abs(thistht ) > detTheta);
+        p1 = partDist(mySeed);
+        p2 = partDist(mySeed);
+    } while(p1 != 0);
 
-    thisphi = angleDist2(mySeed);    // rngen);
-    setPos(thistht, thisphi, hval, dist, xval, yval);
-  } while ( (std::abs(xval) > lx) || (std::abs(yval) > ly));
+    do {
+        do {
+            thistht = angleDist1(mySeed); // rngen);
+        } while(std::abs(thistht) > detTheta);
+
+        thisphi = angleDist2(mySeed); // rngen);
+        setPos(thistht, thisphi, hval, dist, xval, yval);
+    } while((std::abs(xval) > lx) || (std::abs(yval) > ly));
 }
 
-void detNodeCompute(detector& mystrip) {
-  double xloc = 0.0, yloc = 0.0;
-  mystrip.dx = (mystrip.numStripX) ? mystrip.lenX / (double)(mystrip.numStripX + 1) : 0.0; // horizontal
-  mystrip.dy = (mystrip.numStripY) ? mystrip.lenY / (double)(mystrip.numStripY + 1) : 0.0; // vertical
+void detNodeCompute(stripdetector& mystrip)
+{
+    double xloc = 0.0, yloc = 0.0;
+    mystrip.dx = (mystrip.numStripX) ? mystrip.lenX / (double)(mystrip.numStripX + 1) : 0.0; // horizontal
+    mystrip.dy = (mystrip.numStripY) ? mystrip.lenY / (double)(mystrip.numStripY + 1) : 0.0; // vertical
 
-  std::cout << mystrip.name << "  XNum = " << mystrip.numStripX << "  YNum = " << mystrip.numStripY << "  ";
-  std::cout << "dx = " << mystrip.dx << " dy = " << mystrip.dy << std::endl;
+    std::cout << mystrip.name << "  XNum = " << mystrip.numStripX << "  YNum = " << mystrip.numStripY << "  ";
+    std::cout << "dx = " << mystrip.dx << " dy = " << mystrip.dy << std::endl;
 
-  int icnt = 0;
-  if ( mystrip.dx == 0.0 && mystrip.dy != 0.0) {                           //   hozizontal
-    icnt = 0;
-    for (int ynode=0; ynode< mystrip.numStripY; ynode++) {
-      ++icnt;
-      std::vector<double> temp;
-      xloc = -999.99;
-      yloc = -mystrip.halfY + (ynode + 1) * mystrip.dy;
-      temp.push_back(icnt);
-      temp.push_back(xloc);
-      temp.push_back(yloc);
-      detNodes.push_back(temp);
-      temp.clear();
+    int icnt = 0;
+    if(mystrip.dx == 0.0 && mystrip.dy != 0.0) { //   hozizontal
+        icnt = 0;
+        for(int ynode = 0; ynode < mystrip.numStripY; ynode++) {
+            ++icnt;
+            std::vector<double> temp;
+            xloc = -999.99;
+            yloc = -mystrip.halfY + (ynode + 1) * mystrip.dy;
+            temp.push_back(icnt);
+            temp.push_back(xloc);
+            temp.push_back(yloc);
+            detNodes.push_back(temp);
+            temp.clear();
+        }
     }
-  }else if (mystrip.dx != 0.0 && mystrip.dy == 0.0) {                     // vertical
-    icnt = 0;
-    for (int xnode = 0; xnode < mystrip.numStripX; xnode++) {
-      ++icnt;
-      std::vector<double> temp;
-      xloc = -mystrip.halfX + (xnode + 1) * mystrip.dx;
-      yloc = -999.99;
-      temp.push_back(icnt);
-      temp.push_back(xnode);
-      temp.push_back(yloc);
-      detNodes.push_back(temp);
-      temp.clear();
+    else if(mystrip.dx != 0.0 && mystrip.dy == 0.0) { // vertical
+        icnt = 0;
+        for(int xnode = 0; xnode < mystrip.numStripX; xnode++) {
+            ++icnt;
+            std::vector<double> temp;
+            xloc = -mystrip.halfX + (xnode + 1) * mystrip.dx;
+            yloc = -999.99;
+            temp.push_back(icnt);
+            temp.push_back(xnode);
+            temp.push_back(yloc);
+            detNodes.push_back(temp);
+            temp.clear();
+        }
     }
-} else if (mystrip.dx != 0.0 && mystrip.dy != 0.0) {
-    icnt = 0;
-    for (int xnode = 0; xnode < mystrip.numStripX; xnode++) {
-      for (int ynode = 0 ; ynode < mystrip.numStripY; ynode++) {
-        ++icnt;
-        std::vector<double> temp;
-        xloc = -mystrip.halfX + (xnode + 1) * mystrip.dx;
-        yloc = -mystrip.halfY + (ynode + 1) * mystrip.dy;
-        temp.push_back(icnt);
-        temp.push_back(xloc);
-        temp.push_back(yloc);
-        detNodes.push_back(temp);
-        temp.clear();
-      }
+    else if(mystrip.dx != 0.0 && mystrip.dy != 0.0) {
+        icnt = 0;
+        for(int xnode = 0; xnode < mystrip.numStripX; xnode++) {
+            for(int ynode = 0; ynode < mystrip.numStripY; ynode++) {
+                ++icnt;
+                std::vector<double> temp;
+                xloc = -mystrip.halfX + (xnode + 1) * mystrip.dx;
+                yloc = -mystrip.halfY + (ynode + 1) * mystrip.dy;
+                temp.push_back(icnt);
+                temp.push_back(xloc);
+                temp.push_back(yloc);
+                detNodes.push_back(temp);
+                temp.clear();
+            }
+        }
     }
-  }
 }
 
+int checkDetection(double xpos, double ypos, detector det)
+{
+    int chk = 0; // 0 means not detected 1 means detected
+    double xx = 0.0, yy = 0.0;
+    int detType = 0;
+    if(det.dx == 0.0)
+        detType = 0;
+    if(det.dy == 0.0)
+        detType = 1;
+    if(det.dx != 0.0 && det.dy != 0.0)
+        detType = 2;
 
-int checkDetection(double xpos, double ypos, detector det) {
-  int chk = 0; // 0 means not detected 1 means detected
-  double xx = 0.0, yy = 0.0;
-  int detType = 0;
-  if (det.dx == 0.0) detType = 0;
-  if (det.dy == 0.0) detType = 1;
-  if (det.dx != 0.0 && det.dy != 0.0) detType = 2;
+    for(std::vector<std::vector<double>>::iterator it1 = detNodes.begin(); it1 != detNodes.end(); ++it1) {
+        xx = (*it1)[1];
+        yy = (*it1)[2];
 
-  for (std::vector<std::vector<double> >::iterator it1 = detNodes.begin(); it1 !=detNodes.end(); ++it1) {
-    xx = (*it1)[1];
-    yy = (*it1)[2];
-
-    switch (detType) {
-    case 0: chk = isAT(ypos, yy);
-    case 1: chk = isAT(xpos, xx);
-    case 2: chk = (isAT(xpos, xx) && isAT(ypos, yy));
-      if (chk)  std::cout << detType << " " << chk << std::endl;
+        switch(detType) {
+        case 0:
+            chk = isAT(ypos, yy);
+        case 1:
+            chk = isAT(xpos, xx);
+        case 2:
+            chk = (isAT(xpos, xx) && isAT(ypos, yy));
+            if(chk)
+                std::cout << detType << " " << chk << std::endl;
+        }
     }
-  }
-  return chk;
+    return chk;
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,61 @@
 #include "main.h"
 
-int main ( int argc, char** argv )
+int main(int argc, char** argv)
 {
-  // declaration of some important variables
-  int particle1=99, particle2=99;  // 99 means error
-  double detDist = 17.5 * cm2mm; //  10 cm
-  double detSolAngle = 0.0;
-  double detTheta = 0.0; // in radian
-  double detThetaby2 = 0.0;
-  double solAngF1 = 0.0, solAngF2 = 0.0;
-  double theta = 999.9, phi = 999.9;
-  double xpos = 0.0, ypos = 0.0;
-  long NumParticles = 20;  //  1e+8;  // total number of pairs to see
-  long pDetected = 0;    // particles detected by detector
-
+    // declaration of some important variables
+    int particle1 = 99, particle2 = 99; // 99 means error
+    double detDist = 17.467 * cm2mm;    //  10 cm
+    double detSolAngle = 0.0;
+    double detTheta = 0.0; // in radian
+    double detThetaby2 = 0.0;
+    double solAngF1 = 0.0, solAngF2 = 0.0;
+    double theta = 999.9, phi = 999.9;
+    double xpos = 0.0, ypos = 0.0;
+    long NumParticles = 20; //  1e+8;  // total number of pairs to see
+    long pDetected = 0;     // particles detected by detector
 
     // Initialize product particle from target
-  particle alpha("alpha", 4.00150646649, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-  particle tritium("triton", 3.0160492, 0.0, 0.0, 0.0, 0.0, 14949.794e+3, 8481.821e+3);
-  particle deuteron ("deuteron", 2.01410178, 0.0, 0.0, 0.0, 0.0, 13135.720e+3, 2224.52e+3);
-  particle proton("proton", 1.00727646687991, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    particle alpha("alpha", 4.00150646649, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+    particle tritium("triton", 3.0160492, 0.0, 0.0, 0.0, 0.0, 14949.794e+3, 8481.821e+3);
+    particle deuteron("deuteron", 2.01410178, 0.0, 0.0, 0.0, 0.0, 13135.720e+3, 2224.52e+3);
+    particle proton("proton", 1.00727646687991, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 
-  // initialize detector
-  detector strip("strip", 50, 50, 1.55, 16, 16); // 5 cm X 5 cm X 0.155 cm and 16 X 16 lines
-  detector eDet ("eDet", 50, 50, 1.55, 0, 16); // 5cm X 5cm X 0.155cm 16 horizontal
-  detector delEDet("delEDet", 50, 50, 50e-3, 16, 0); // 5cm X 5cm X 50e-4cm 16 vertical
-  detNodeCompute(strip);
-  detNodeCompute(eDet);
-  detNodeCompute(delEDet);
+    // initialize detector
+    stripdetector strip("strip", 49.5, 49.5, 1.55, 0.1, 16, 16);     // 5 cm X 5 cm X 0.155 cm and 16 X 16 lines
+    stripdetector eDet("eDet", 49.5, 49.5, 1.50, 0.1, 0, 16);        // 5cm X 5cm X 0.155cm 16 horizontal
+    stripdetector delEDet("delEDet", 49.5, 49.5, 65e-3, 0.1, 16, 0); // 5cm X 5cm X 50e-4cm 16 vertical
+    detNodeCompute(strip);
+    detNodeCompute(eDet);
+    detNodeCompute(delEDet);
 
+    // std::cout << " strip area = " << strip.SArea() << std::endl;
+    std::cout << " Strip limit :->  X = " << -strip.halfX << " mm : " << strip.halfX
+              << " mm    and  Y = " << -strip.halfY << " mm : " << strip.halfY << " mm " << std::endl;
 
-  // std::cout << " strip area = " << strip.SArea() << std::endl;
-  std::cout << " Strip limit :->  X = " << -strip.halfX << " mm : " << strip.halfX
-	    << " mm    and  Y = " << -strip.halfY << " mm : " << strip.halfY << " mm " << std::endl;
-  
-  // detSolAngle = strip.SArea()/x2(detDist);    // steradian
-  solAngF1 = 0.5 * sqrt(4.0 * x2(detDist) +  x2(strip.lenX) + x2(strip.lenY));     // physically slanting length
-  solAngF2 = strip.SArea() /sqrt((x2(strip.lenX) + x2(solAngF1)) * (x2(strip.lenY) + x2(solAngF1)));
-  detSolAngle =  4.0 * asin(solAngF2);
-  // detTheta = 0.5 * detSolAngle;   
-  detTheta = asin(strip.halfX / solAngF1);    // acos(detDist / solAngF1);                     // acos(1.0 - detSolAngle/PI2);
-  detThetaby2 = 0.5 * detTheta;
+    // detSolAngle = strip.SArea()/x2(detDist);    // steradian
+    solAngF1 = 0.5 * sqrt(4.0 * x2(detDist) + x2(strip.lenX) + x2(strip.lenY)); // physically slanting length
+    solAngF2 = strip.SArea() / sqrt((x2(strip.lenX) + x2(solAngF1)) * (x2(strip.lenY) + x2(solAngF1)));
+    detSolAngle = 4.0 * asin(solAngF2);
+    // detTheta = 0.5 * detSolAngle;
+    detTheta =
+        asin(strip.halfX / solAngF1); // acos(detDist / solAngF1);                     // acos(1.0 - detSolAngle/PI2);
+    detThetaby2 = 0.5 * detTheta;
 
-  //  std::cout << " solid angle = " << detSolAngle << "   theta = " << detTheta << "  phi = " << PI2 << std::endl;
+    //  std::cout << " solid angle = " << detSolAngle << "   theta = " << detTheta << "  phi = " << PI2 << std::endl;
 
     // generate particles with specific energy
     // and
     // within a certain solid angle
-  for (int icnt = 0; icnt < NumParticles; icnt++) {
-    do {
-      gen_Particle (particle1, particle2,  detTheta, strip.halfX, strip.halfY, solAngF1, detDist, xpos, ypos);
-    } while (particle1 == 99 || particle2 == 99);
-    // std::cout << particleArray[particle1] << " --- " << particleArray[particle2] << " x = " << xpos << " y = " << ypos << std::endl;
-    checkDetection(xpos, ypos, eDet);
-    //   if (checkDetection(xpos, ypos, eDet)) 
-    //  std::cout << particleArray[particle1] << " and " << particleArray[particle2] << " detected....." << std::endl;   
-  }
+    for(int icnt = 0; icnt < NumParticles; icnt++) {
+        do {
+            gen_Particle(particle1, particle2, detTheta, strip.halfX, strip.halfY, solAngF1, detDist, xpos, ypos);
+        } while(particle1 == 99 || particle2 == 99);
+        // std::cout << particleArray[particle1] << " --- " << particleArray[particle2] << " x = " << xpos << " y = " <<
+        // ypos << std::endl;
+        checkDetection(xpos, ypos, eDet);
+        //   if (checkDetection(xpos, ypos, eDet))
+        //  std::cout << particleArray[particle1] << " and " << particleArray[particle2] << " detected....." <<
+        //  std::endl;
+    }
     return 0;
 }
